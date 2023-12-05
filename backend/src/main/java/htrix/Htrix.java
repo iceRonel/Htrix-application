755e902a6ae514eff88a3c0dc7aae18f54d372ac
ronel kontchou <ronel.kontchou@hotmail.fr>
2023-12-05 18:28:29 +0100

    init  htrix project with the backend powered by spring

---
+++ backend/src/main/java/htrix/Htrix.java
@@ -0,0 +1,13 @@
+package htrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Htrix {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Htrix.class, args);
+	}
+
+}
---
+++ backend/src/test/java/htrix/HtrixTests.java
@@ -0,0 +1,13 @@
+package htrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtrixTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
